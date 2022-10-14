e6d15695a89577d1de90ae93fd751e76fd70c67e
unknown <thongnguyennqt@gmail.com>
2022-10-14 10:14:17 +0700

    changed url

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://tmtuan:Zdub8FnkJh9cTrsm@cluster0.e6jqisa.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://crud:crud_123@cluster0.kaunka5.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
